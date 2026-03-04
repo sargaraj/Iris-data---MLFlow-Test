75464781696f1afd8be542101c17ea7600fe9823
sargaraj .T <66464215+sargaraj@users.noreply.github.com>
2026-03-04 09:52:53 +0530

    Add files via upload

---
+++ fastapi_main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"message": "Item created successfully", "data": item}
